a875e5c57c25a235af859085303cdd3093595139
JavierMaca <ejmaca@unimayor.edu.co>
2021-03-22 00:05:49 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-simple-angular';
-}
+
+employeeArray: Employee[] = [
+  {id: 1, name: 'Javier', country: 'Colombia'},
+  {id: 2, name: 'Ana', country: 'Colombia'},
+  {id: 3, name: 'Pedro', country: 'Colombia'}
+];
+
+selectEmployee: Employee = new Employee();
+
+AddOrEdit(){
+  if (this.selectEmployee.name == null) {
+    alert('Debes rellenar los campos!!!')
+    } else if (this.selectEmployee.id == 0)
+   {
+     
+     this.selectEmployee.id = this.employeeArray.length + 1;
+     this.employeeArray.push(this.selectEmployee)  
+     console.log('ddd');
+   } 
+  this.selectEmployee = new Employee();
+ }  
+
+ openForEdit(employ: Employee){
+   this.selectEmployee = employ;
+ }
+
+ delete(){
+   if (confirm('Are you sure you want to delete it?')) {
+     this.employeeArray = this.employeeArray.filter( x => x != this.selectEmployee);
+
+     this.selectEmployee = new Employee();
+   }
+ }
+
+}// fin class
